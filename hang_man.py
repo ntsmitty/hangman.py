e29b0c4ef395abc7ceb3ee00a0f5490507c7d1ce
Nicholas Smith <nicktsmith7@gmail.com>
2017-07-09 20:43:53 -0700

    Update hang_man.py

--- hang_man.py
+++ hang_man.py
@@ -6,24 +6,22 @@ class Hangman(object):
 
     secret_word = Counter(getword_mod.get_word())
     print secret_word
+    
+    #correct_guesses should not exceed the length of the secret_word, when it is equal - you win.
     def __init__(self):
         self.used_letters = ' '
         self.correct_guesses = 0
+        self.user_guess = raw_input('Enter a letter: ')
 
-
-    def check_user_guess(self):
-        if  user_guess in secret_word and user_guess not in used_letters:
+    def validate_user_guess(self):
+        if self.user_guess in self.secret_word and self.user_guess not in self.used_letters:
             self.correct_guesses += self.secret_word[self.user_guess]
+            self.used_letters += ' '.join(self.user_guess)
         elif self.user_guess not in self.secret_word:
             self.used_letters += ' '.join(self.user_guess)
-            print 'This is the: %r' % self.correct_guesses
+        return self.correct_guesses
 
-#     def play_game(self, user_guess):
-#         user_guess = raw_input('Enter a letter: ')
-#         while self.correct_guesses < sum(Hangman.values(secret_word)):
-#             user_guess = raw_input('Please enter a guess: ')
-            
+
 
 game = Hangman()
-game.check_user_guess()
-game.play_game(game)
+game.validate_user_guess())
